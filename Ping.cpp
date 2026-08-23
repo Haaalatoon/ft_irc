822a603ffc668fdf5c937bc94fcda0732c8120af
Auratoon <aura.rhln@gmail.com>
2026-08-23 19:31:33 +0100

    Branch for Person B: Channels & Commands files only

--- Ping.cpp
+++
@@ -1,7 +0,0 @@
-#include "Server.hpp"
-
-void Server::handlePing(int fd, std::vector<std::string> args) {
-    std::string token = args.empty() ? "" : args[0];
-    std::string response = ":" + serverName + " PONG " + serverName + " :" + token + "\r\n";
-    sendRaw(fd, response);
-}
--- Utils.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_HPP
-#define UTILS_HPP
-
-#include <string>
-
-int validatePort(const std::string& av);
-
-#endif
